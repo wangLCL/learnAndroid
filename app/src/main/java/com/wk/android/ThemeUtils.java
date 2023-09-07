8f2bf2f9129acfad956a715691f31eed547ec236
kangwang <2818815189@qq.com>
2023-09-07 23:59:17 +0800

    隐藏底部导航，不隐藏顶部状态

--- app/src/main/java/com/wk/android/ThemeUtils.java
+++ app/src/main/java/com/wk/android/ThemeUtils.java
@@ -48,8 +48,15 @@ public class ThemeUtils {
     }
 
     public void hideNav(){
+//        View decorView = context.getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        decorView.setSystemUiVisibility(uiOptions);
+
         View decorView = context.getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                ;
         decorView.setSystemUiVisibility(uiOptions);
     }
 
