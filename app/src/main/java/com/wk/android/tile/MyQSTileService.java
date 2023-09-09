19a83487d3ee64d664d821adddf83400e31f0cea
kangwang <2818815189@qq.com>
2023-09-09 08:44:19 +0800

    设置中的菜单

---
+++ app/src/main/java/com/wk/android/tile/MyQSTileService.java
@@ -0,0 +1,40 @@
+package com.wk.android.tile;
+
+import android.os.Build;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class MyQSTileService extends TileService {
+
+    // Called when the user adds your tile.
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    // Called when your app can update your tile.
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    // Called when your app can no longer update your tile.
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    // Called when the user taps on your tile in an active or inactive state.
+    @Override
+    public void onClick() {
+        super.onClick();
+    }
+
+    // Called when the user removes your tile.
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+}
